29906483cbbcd046f684be4b6840962556c938e4
uddugarg <udivyy.99@gmail.com>
2025-04-10 13:30:11 +0530

    feat: update authentication layout and branding to reflect new application name

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,10 +1,15 @@
+import { isAuthenticated } from "@/lib/actions/auth.action";
+import { redirect } from "next/navigation";
 import React from "react";
 
 type Props = {
   children: React.ReactNode;
 };
 
-const AuthLayout = ({ children }: Props) => {
+const AuthLayout = async ({ children }: Props) => {
+  const isUserAuthenticated = await isAuthenticated();
+  if (isUserAuthenticated) redirect("/");
+
   return <div className="auth-layout">{children}</div>;
 };
 
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,21 +1,23 @@
+import { isAuthenticated } from "@/lib/actions/auth.action";
 import Image from "next/image";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 import React from "react";
 
 type Props = {
   children: React.ReactNode;
 };
 
-const RootLayout = ({ children }: Props) => {
-  // const isUserAuthenticated = await isAuthenticated();
-  // if (!isUserAuthenticated) redirect("/sign-in");
+const RootLayout = async ({ children }: Props) => {
+  const isUserAuthenticated = await isAuthenticated();
+  if (!isUserAuthenticated) redirect("/sign-in");
 
   return (
     <div className="root-layout">
       <nav>
         <Link href="/" className="flex items-center gap-2">
           <Image src="/logo.svg" alt="MockMate Logo" width={38} height={32} />
-          <h2 className="text-primary-100">PrepWise</h2>
+          <h2 className="text-primary-100">PrepAI</h2>
         </Link>
       </nav>
 
